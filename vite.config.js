a3c1648d37d59b0d460374b5e9870dd05b792d43
Sergio Garcia <sergio.garciia.munoz@gmail.com>
2023-09-01 16:02:56 +0200

    Update config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/fake-store"
   test: {
     globals: true,
     environment: "jsdom",
